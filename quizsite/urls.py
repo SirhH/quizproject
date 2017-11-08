e19688a2a8905ee851e0d8523ed66a9c907720ce
SirhH <maria.hansson@kundo.se>
2017-11-08 10:06:14 +0100

    dynamic data in results

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -19,6 +19,6 @@ from quiz import views
 urlpatterns = [
     path('', views.startpage),
     path('quiz/<int:quiz_number>/', views.quiz),
-    path('guiz/<int:quiz_number>/question/<int:question_number>/', views.question),
+    path('quiz/<int:quiz_number>/question/<int:question_number>/', views.question),
     path('quiz/<int:quiz_number>/completed', views.completed),
 ]
