54e3c64b793f52976acbb516978ec7899045755b
SirhH <maria.hansson@kundo.se>
2017-11-08 09:29:51 +0100

    dynamic data in quiz.html

--- quiz/views.py
+++ quiz/views.py
@@ -21,13 +21,17 @@ quizzes = [
 
 def startpage(request):
     context = {
-        "quizzes": quizzes,
+        'quizzes': quizzes,
     }
     return render(request, 'start.html', context)
 
 
 def quiz(request, quiz_number):
-    return render(request, 'quiz.html')
+    context = {
+        'quiz': quizzes[quiz_number -1],
+        'quiz_number': quiz_number,
+    }
+    return render(request, 'quiz.html', context)
 
 
 def question(request, quiz_number, question_number):
