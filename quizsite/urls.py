6dbdf956d68b9cc17331177462fca69502dc331b
SirhH <maria.hansson@kundo.se>
2017-11-09 10:12:25 +0100

    urls.py admin

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.urls import path
 from quiz import views
+from django.contrib import admin
 
 urlpatterns = [
     path('', views.startpage, name='start_page'),
     path('quiz/<int:quiz_number>/', views.quiz, name='quiz_page'),
     path('quiz/<int:quiz_number>/question/<int:question_number>/', views.question, name='question_page'),
     path('quiz/<int:quiz_number>/completed', views.completed, name='result_page'),
+    path('admin/', admin.site.urls),
 ]
