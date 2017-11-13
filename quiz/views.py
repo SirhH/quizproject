5a4a7a995a5a34b15c3bc985039d81d8a37b6660
SirhH <maria.hansson@kundo.se>
2017-11-13 10:28:19 +0100

    Front-end changes: placeholder img and css re-work

--- quiz/views.py
+++ quiz/views.py
@@ -4,28 +4,6 @@ from django.shortcuts import redirect
 from django.http import Http404
 
 # Create your views here.
-quizzes = [
-    {
-        'quiz_number': 1,
-        'name': 'Let me guess your favourite color',
-        'description': 'Jag tror att jag vet vilken färg du gillar'
-    },
-    {
-        'quiz_number': 2,
-        'name': 'Are you in a good mood?',
-        'description': 'See if you are having a good day'
-    },
-    {
-        'quiz_number': 3,
-        'name': 'Guess my card',
-        'description': 'I am thinking of a card, can you find out?'
-    },
-    {
-        'quiz_number': 4,
-        'name': 'Let me guess your card',
-        'description': 'Can I guess what card you are thinking of?'
-    },
-]
 
 
 def startpage(request):
@@ -42,10 +20,10 @@ def quiz(request, quiz_number):
         raise Http404
     if quiz_number > l_quiz.quiz_number:
         raise Http404
-
     context = {
         'quiz': l_quiz,
         'quiz_number': quiz_number,
+
     }
     return render(request, 'quiz.html', context)
 
@@ -59,7 +37,7 @@ def question(request, quiz_number, question_number):
         raise Http404
     l_questions = l_quiz.questions.all()
     l_question = l_questions[question_number - 1]
-
+    last_question_number = l_questions.count()
     context = {
         'question_number': question_number,
         'question': l_question.question,
@@ -67,6 +45,7 @@ def question(request, quiz_number, question_number):
         'answer2': l_question.answer2,
         'answer3': l_question.answer3,
         'quiz_number': quiz_number,
+        'last_question_number': last_question_number,
     }
     return render(request, 'question.html', context)
 
